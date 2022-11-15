4b6b02226c93693c9e5a7593ffcb290d7a85cf61
Seyolas <saritas.seydi@gmail.com>
2022-11-16 01:08:19 +0300

    axios config

--- components/UploadImage.js
+++ components/UploadImage.js
@@ -1,84 +1,61 @@
 import { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-
+import axios from 'axios';
 import { FiUpload } from 'react-icons/fi';
+import React, { useCallback, useRef } from "react";
+import ReactCanvasConfetti from "react-canvas-confetti";
 
 const UploadImage = () => {
 
-    const {
-        acceptedFiles,
-        fileRejections,
-        getRootProps,
-        getInputProps,
-        open,
-    } = useDropzone({
-        accept: {
-            'image/jpeg': [],
-            'image/png': []
-        },
-        noClick: true,
-        noKeyboard: true
-    });
-    const acceptedFileItems = acceptedFiles.map(file => (
-        <li key={file.path}>
-            {file.path} - {file.size} bytes
-        </li>
-    ));
+
+
+    // a local state to store the currently selected file.
+    const [selectedFile, setSelectedFile] = useState(null);
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const formData = new FormData();
+        formData.append("selectedFile", selectedFile);
+        try {
+            const response = await axios({
+                method: "post",
+                url: "/api/upload/file",
+                data: formData,
+                headers: { "Content-Type": "multipart/form-data" },
+            });
+        } catch (error) {
+            console.log(error)
+            alert("errorr")
+        }
+    }
+
+    const handleFileSelect = (event) => {
+        setSelectedFile(event.target.files[0])
+    }
 
 
     return (
-        <div className="container">
-            <div {...getRootProps({ className: 'dropzone' })}>
-                <div className='main'>
-                    <input {...getInputProps()} />
-                    <p>Sürükle bırak veya tıkla</p>
-                    <button className='button' type="button" onClick={open}>
-                        <FiUpload /> Yükle
-                    </button>
-                    <em>(Sadece *.jpeg ve *.png kabul edilir)</em>
-                    <ul>{acceptedFileItems}</ul>
-                    <ul>{fileRejections}</ul>
-                </div>
-            </div>
+        <form onSubmit={handleSubmit}>
+            <main>
 
+                <input type="file" onChange={handleFileSelect} accept=".jpg,.jpeg" />
+                <input type="submit" value="Upload File" />
+            </main>
             <style jsx>{`
-                
-                    .container{
-                        background-color:#f1f5f9;
-                        width:400px;
-                        max-width:400px;
-                        height:300px;
-                        max-height:300px;
-                        margin:auto;
-                        margin-top:40px;
-                        border:dotted 5px #e2e8f0;
-                        display:flex;
-                        align-items:center;
-                        justify-content:center;
-                    }
-                    button{
-                        width:100px;
-                        background-color:#3b82f6;
-                        color:#fff;
-                        border-radius:3px;
-                        padding:5px 3px;
-                        font-weight:800;
-                        cursor:pointer;
-                        margin:10px 0;
-                    }
-                    .main{
-                        display:flex;
-                        flex-direction:column;
-                        align-items:center;
-                        justify-content:center;
-                    }
-                    .input-zone{
-                        background-color:red;
-                        width:1000px
-                    }
-                `}
+        main{
+            width:500px;
+            margin:auto;
+            margin-top:30px;
+            background-color:#f1f5f9;
+            display:flex;
+            flex-direction:column;
+        }
+        
+        `}
+
             </style>
-        </div>
+        </form>
     )
 }
 
