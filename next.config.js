8500e3b02ad42dd940741541567798ee54edac4e
Seyolas <saritas.seydi@gmail.com>
2022-12-03 15:01:13 +0300

    x-content-type-options ve strict-transport-security güvenlik açıkları giderildi

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
+
+const securityHeaders = [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  }
+
+]
+
 module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+
   reactStrictMode: true,
 }
